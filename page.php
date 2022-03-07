a7c5ebefbcd14bb7c2f685f5f43ce6739f16846f
Lokisn <2035137@cgmatane.qc.ca>
2022-03-07 17:56:25 -0500

    Ajout page generic

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php get_header()?>
+
+    <div>
+
+        <section>
+            <?php get_template_part( 'templates/generic-loop'); ?>
+        </section>
+
+        <section>
+            <?php get_sidebar(  ); ?>
+        </section>
+
+    </div>
+
+<?php get_footer( ) ?>
