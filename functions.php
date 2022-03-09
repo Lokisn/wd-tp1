6f07d0d23f89e6b9ff6be5e2441ea945413f0abb
Noa Prevel <suplokier@gmail.com>
2022-03-08 20:59:17 -0500

    Ajout du support de thème, déclaration des menus, widgets dans la page functions

--- functions.php
+++ functions.php
@@ -9,6 +9,48 @@ function concerto_enqueue_styles() {
     wp_enqueue_style( 'maincss', get_template_directory_uri() . '/styles/main.css', array('cookiefont') );
 }
 
-add_action( 'wp_enqueue_scripts', 'concerto_enqueue_styles' )
+add_action( 'wp_enqueue_scripts', 'concerto_enqueue_styles' );
+
+// Supports de thème :
+function concerto_add_theme_support(){
+    add_theme_support( 'post-thumbnails' );
+
+    // Titre adaptatif pour la page 
+    add_theme_support( 'title-tag' );
+}
+
+add_action( 'after_setup_theme', 'concerto_add_theme_support' );
+
+// Déclarer les menus : 
+function concerto_register_menus(){
+
+    register_nav_menus( array(
+        'main-menu' => __('Menu principal')
+    ));
+}
+
+add_action( 'after_setup_theme', 'concerto_register_menus');
+
+// Widgets : 
+function concerto_widgets_init($id){
+    register_sidebar( array(
+        'name' => 'Widget 1',
+        'id' => 'widget-1',
+        'description' => 'Widget affiché en haut du Sidebar',
+        'before_widget' => '<div class="side-widget">',
+        'after_widget' => '</div>'
+    ));
+
+    register_sidebar( array(
+        'name' => 'Widget 2',
+        'id' => 'widget-2',
+        'description' => 'Widget affiché en bas du Sidebar',
+        'before_widget' => '<div class="side-widget">',
+        'after_widget' => '</div>'
+    ));
+}
+
+add_action( 'widgets_init', 'concerto_widgets_init' );
+
 
 ?>
--- index.php
+++ index.php
@@ -1,32 +1,39 @@
-<?php get_header()?>
+<?php get_header() ?>
 
-    <?php if(is_archive());?>
+    <?php if(is_archive()) : ?>
 
         <h2><?php echo esc_html(get_the_category()[0]->name) ?></h2>
 
-    <?php else :?>
+    <?php else : ?>
 
         <h2>Accueil</h2>
 
-    <?php endif;?>
+    <?php endif; ?>
+    
+        <div class="flex">
+            <section class="trois-quart">
 
-        <div>
-            <section>
+            <?php if(have_posts( )) : ?>
+            <?php while(have_posts( )) : the_post(  ); ?>
 
-                <?php if(have_posts( )) : ?>
-                <?php while(have_posts( )) : the_post(  ); ?>
+                <article class="flex">
 
-                <article>
+                    <!-- L'image de l'article -->
+                    <div class="image-article quart">
 
-                    <div>
-                        <?php if(has_post_thumbnail( )) : ?>
-                            <?php the_post_thumbnail('small', ['class' => 'fluide']);?>
-                        <?php endif;?>
+                        <?php if(has_post_thumbnail(  )) : ?>
+                            
+                            <?php the_post_thumbnail('small', ['class' => 'fluide']); ?>
+
+                        <?php endif; ?>
+
                     </div>
 
-                    <div>
-                        <h3><?php the_title();?></h3>
-                        <p> <?php the_excerpt();?> <a href="<?=the_permalink();?>"> En savoir plus... </a> </p>
+                    <div class="texte-article trois-quart">
+                        <!-- Récupération des articles (titre, contenu)-->
+                        <h3><?php the_title(); ?></h3>
+                        <p><?php the_excerpt(); ?> <a href="<?=the_permalink();?>"> En savoir plus... </a> </p>
+                        <p><?php the_tags()?></p>
                     </div>
 
                 </article>
@@ -35,4 +42,19 @@
             <?php endif; ?>
 
             </section>
+            <!--À MODIFIER !-->
+            <section class="quart sidebar">
+                <article>
+                    <h3>Sidebar</h3>
+                    <div class="widget-container">
+                        <!-- widget -->
+                    </div>
+                    <div class="widget-container">
+                        <!-- widget -->
+                    </div>
+                </article>
+            </section>
         </div>
+
+<?php get_footer( ) ?>
+
