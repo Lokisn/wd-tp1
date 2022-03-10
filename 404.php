03bae3eb98a6129410978bcaeaaa8ce6ae9fb4a0
Noa Prevel <suplokier@gmail.com>
2022-03-10 01:12:50 -0500

    Correction page 404

--- 404.php
+++ 404.php
@@ -6,9 +6,9 @@ get_header();
 
 	<div>
 
-		<h1><?php _e( 'Page inexistante'); ?></h1>
+		<h1><?php echo( 'Page inexistante'); ?></h1>
 
-		<div><p><?php _e( "La page que vous recherchez n'a pas pu être trouvée. Elle a peut-être été supprimée, renommée ou n'existait pas à l'origine."); ?></p></div>
+		<div><p><?php echo( "La page que vous recherchez n'a pas pu être trouvée. Elle a peut-être été supprimée, renommée ou n'existait pas à l'origine."); ?></p></div>
             
 	    </div>
 
