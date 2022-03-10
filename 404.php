0b1e46ed62e6423dc6c1bd5170df79278cf87ea4
Noa Prevel <suplokier@gmail.com>
2022-03-10 01:10:12 -0500

    Ajout de la page 404

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+?>
+
+<main>
+
+	<div>
+
+		<h1><?php _e( 'Page inexistante'); ?></h1>
+
+		<div><p><?php _e( "La page que vous recherchez n'a pas pu être trouvée. Elle a peut-être été supprimée, renommée ou n'existait pas à l'origine."); ?></p></div>
+            
+	    </div>
+
+</main>
+
+<?php
+get_footer();
